54dfb4eac6e969c067297819ef4a851d1bedf168
Esteban Martinena <orensbruli@gmail.com>
2025-11-20 15:36:52 +0100

    Adds Debian package build script
    Introduces a script for building Debian packages, streamlining the release process.
    
    The script automates building `fpsdk_common`, `fpsdk_ros1`, and `fixposition_driver_lib` Debian packages.
    It also addresses the missing dependency on `ros-noetic-fpsdk-common` and `ros-noetic-fpsdk-ros1` in the `fixposition_driver_lib` control file.

--- .circleci/build_debian_packages.sh
+++ .circleci/build_debian_packages.sh
@@ -1,6 +1,21 @@
+#!/bin/bash
 set -x
 ls -lah
+apt update
 git submodule sync
 git submodule update --init --recursive
 cd fixposition-sdk
 greenzie-release changelog -r "noetic" --with-submodules
+apt -y build-dep ./fpsdk_common
+apt -y build-dep ./fpsdk_ros1
+cd fpsdk_common && debuild --no-tgz-check -b --no-sign --lintian-opts --suppress-tags dir-or-file-in-opt
+apt -y install ../ros-noetic-fpsdk-common*.deb
+cd ../fpsdk_ros1 && debuild --no-tgz-check -b --no-sign --lintian-opts --suppress-tags dir-or-file-in-opt
+apt -y install ../ros-noetic-fpsdk-ros1*.deb
+cd ../../
+cd fixposition_driver_lib/
+greenzie-release changelog -r "noetic" --with-submodules
+apt -y build-dep .
+debuild --no-tgz-check -b --no-sign --lintian-opts --suppress-tags dir-or-file-in-opt
+
+
